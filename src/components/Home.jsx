88af703d79638059be45416e8a196a893c788c38
sudeys <sudeysimaxamud5@gmail.com>
2024-10-04 19:49:41 +0300

    New

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,22 @@
 import { Link } from "react-router-dom"
 import { useState } from "react"
 import DOC from "../doc/Sudeys_Mohamud.pdf"
+import logo from "../images/Screenshot_2024-10-01_at_1.21.59_PM-removebg-preview.png"
+import ME from "../images/Untitled-NJXyBb8Qp-transformed 2.jpg"
+import LINE from "../images/separatorBlack 1.png"
+import HTML from "../images/vecteezy_stylized-3d-html-logo-design_12697299 (1).png"
+import CSS from "../images/vecteezy_3d-css-icon-design_12697301.png"
+import JAVA from "../images/vecteezy_3d-javascript-logo-design_12697298.png"
+import TAILWIND from "../images/Screenshot_2024-10-02_at_5.06.37_PM-removebg-preview.png"
+import React from "../images/vecteezy_3d-icon-of-atom_20033019.png"
+import nodeJs from "../images/Screenshot_2024-10-02_at_5.02.56_PM-removebg-preview.png"
+import MONGO from "../images/Screenshot_2024-10-02_at_5.10.51_PM-removebg-preview.png"
+import EXPRESS from "../images/Screenshot_2024-10-02_at_5.41.39_PM-removebg-preview.png"
+import word from "../images/Screenshot_2024-10-02_at_7.40.09_PM-removebg-preview.png"
+import APP from "../images/Screenshot_2024-10-02_at_7.49.06_PM-removebg-preview.png"
+import ENGLISH from "../images/Screenshot_2024-10-02_at_8.14.18_PM-removebg-preview.png"
+import ARABIC from "../images/Screenshot_2024-10-02_at_8.12.11_PM-removebg-preview.png"
+
 
 
 
@@ -17,7 +33,7 @@ function Home (){
         <section id="Home" >
         <div className="" >
                 <div  className=" bg-black  sm:h-[80px]  h-full w-full   flex  sm:justify-around  " >
-                    <img id="UL" className= " h-[50px]  mt-[10px] sm:ml-[0px] ml-[30px] sm:pb-0 pb-[10px]    " src="src/images/Screenshot_2024-10-01_at_1.21.59_PM-removebg-preview.png" alt="" />
+                    <img id="UL" className= " h-[50px]  mt-[10px] sm:ml-[0px] ml-[30px] sm:pb-0 pb-[10px]  " src={logo} alt="" />
                     <i onClick={handleOpen}   style={{display :Open ==true? "none" :""  }} class="fa-solid fa-bars-staggered text-white sm:hidden absolute  text-[30px] top-[20px]   right-5  "></i>
                     <i onClick={handleClose}  style={{display :Open ==true? "block" :""  }}   class="fa-solid fa-x text-white hidden absolute  text-[30px] top-[20px]   right-5  "></i>
                     <ul id="UL" style={{display :Open ==true? "block" :""  }}   className="text-[white] sm:flex hidden ml-[-70px]   sm:ml-[100px] sm:mt-[0px] mt-[80px] gap-[70px]  items-center sm:gap-[50px]  " >
@@ -41,7 +57,7 @@ function Home (){
                         </div>
                         
                         <div>
-                            <img  id="me" className="    absolute sm:h-[650px] h-[400px] overflow-visible sm:top-[100px]   top-[300px]  sm:left-[700px] sm:bottom-[110px] left-[5px]   " src="src/images/Untitled-NJXyBb8Qp-transformed 2.jpg" alt="" />
+                            <img  id="me" className="    absolute sm:h-[650px] h-[400px] overflow-visible sm:top-[100px]   top-[300px]  sm:left-[700px] sm:bottom-[110px] left-[5px]   " src={ME} alt="" />
                         </div>
                 </div>
 
@@ -57,7 +73,7 @@ function Home (){
                     <h1 className="  font-Body text-[30px] sm:text-[40px] font-semibold sm:h-[100px] h-[60px] rounded-[2px]   w-[200px] sm:w-[400px] sm:pt-[15px] ml-[100px]  sm:ml-[530px]  text-center border-[4px] border-black  " >ABOUT ME</h1>
                             <p className="  sm:font-Body sm:w-[550px] w-[300px] ml-[50px]  sm:ml-[450px] mt-[30px] text-[15px] font-medium  text-justify sm:text-center   " >
                             I am a student and a passionate full-stack web developer. I love building both front-end and back-end solutions that bring ideas to life. In addition to my technical skills, I can speak three languages, which helps me connect with people from different cultures and backgrounds.</p>
-                    <img className=" ml-[120px] sm:ml-[640px] mt-[20px]  " src="src/images/separatorBlack 1.png" alt="" />    
+                    <img className=" ml-[120px] sm:ml-[640px] mt-[20px]  " src={LINE} alt="" />    
                </div>
                <div className=" sm:flex sm:justify-around mt-[50px]  " >
 
@@ -80,7 +96,7 @@ function Home (){
 
 
             </div>
-            <img className=" sm:ml-[640px] ml-[120px] mt-[150px] pb-[100px]  " src="src/images/separatorBlack 1.png" alt="" />
+            <img className=" sm:ml-[640px] ml-[120px] mt-[150px] pb-[100px]  " src={LINE} alt="" />
 
         </section>
         <section id="Skills" >
@@ -120,35 +136,35 @@ function Home (){
                       
                         <div className="grid sm:grid-cols-4 grid-cols-3 gap-[10px] sm:ml-[300px]  ml-[30px]   " >
                             <div>
-                                <img className="sm:h-[80px]   h-[50px]  hover:scale-110 duration-150     " src="src/images/vecteezy_stylized-3d-html-logo-design_12697299 (1).png" alt="" />
+                                <img className="sm:h-[80px]   h-[50px]  hover:scale-110 duration-150     " src={HTML} alt="" />
                                 <h1 className=" mt-[20px] font-semibold  text-black  " >HTML</h1>
                             </div>
                             <div>
-                                <img className="sm:h-[80px]  h-[50px]   hover:scale-110 duration-150    " src="src/images/vecteezy_3d-css-icon-design_12697301.png" alt="" />
+                                <img className="sm:h-[80px]  h-[50px]   hover:scale-110 duration-150    " src={CSS} alt="" />
                                 <h1 className=" mt-[20px] font-semibold  text-black  " >CSS</h1>
                             </div>
                             <div>
-                                <img className="sm:h-[80px]  h-[50px]   hover:scale-110 duration-150    " src="src/images/vecteezy_3d-javascript-logo-design_12697298.png" alt="" />
+                                <img className="sm:h-[80px]  h-[50px]   hover:scale-110 duration-150    " src={JAVA} alt="" />
                                 <h1 className=" mt-[20px] font-semibold text-black  " >JavaScript</h1>
                             </div>
                             <div>
-                                <img className="sm:h-[80px]  h-[50px]   hover:scale-110 duration-150    " src="src/images/Screenshot_2024-10-02_at_5.06.37_PM-removebg-preview.png" alt="" />
+                                <img className="sm:h-[80px]  h-[50px]   hover:scale-110 duration-150    " src={TAILWIND} alt="" />
                                 <h1 className=" mt-[20px] font-semibold  text-black  " >Tailwind Css</h1>
                             </div>
                             <div>
-                                <img className="sm:h-[80px]  h-[50px]   hover:scale-110 duration-150    " src="src/images/vecteezy_3d-icon-of-atom_20033019.png" alt="" />
+                                <img className="sm:h-[80px]  h-[50px]   hover:scale-110 duration-150    " src={React} alt="" />
                                 <h1 className=" mt-[20px] font-semibold  text-black  " >React</h1>
                             </div>
                             <div>
-                                <img className="sm:h-[80px]  h-[50px]   hover:scale-110 duration-150    " src="src/images/Screenshot_2024-10-02_at_5.02.56_PM-removebg-preview.png" alt="" />
+                                <img className="sm:h-[80px]  h-[50px]   hover:scale-110 duration-150    " src={nodeJs} alt="" />
                                 <h1 className=" mt-[20px] font-semibold  text-black  " >NodeJS </h1>
                             </div>
                             <div>
-                                <img className="sm:h-[100px]  h-[50px]   hover:scale-110 duration-150    " src="src/images/Screenshot_2024-10-02_at_5.10.51_PM-removebg-preview.png" alt="" />
+                                <img className="sm:h-[100px]  h-[50px]   hover:scale-110 duration-150    " src={MONGO} alt="" />
                                 <h1 className=" mt-[20px] font-semibold  text-black  " > MongoDB </h1>
                             </div>
                             <div>
-                                <img className="sm:h-[80px]  h-[50px]   hover:scale-110 duration-150    " src="src/images/Screenshot_2024-10-02_at_5.41.39_PM-removebg-preview.png" alt="" />
+                                <img className="sm:h-[80px]  h-[50px]   hover:scale-110 duration-150    " src={EXPRESS}  alt="" />
                                 <h1 className=" mt-[20px] font-semibold  text-black  " > Express Js </h1>
                             </div>
                         </div>
@@ -158,12 +174,12 @@ function Home (){
                     <h1 className=" sm:ml-[100px] ml-[20px] text-[25px] font-semibold mt-[20px]  sm:mt-[10px]  text-black " >LEARNING:</h1>
                     <div className="grid grid-cols-3 gap-[20px] sm:ml-[300px] ml-[30px] sm:pt-0 pt-[30px]    " >
                             <div  >
-                                <img className="sm:h-[80px] h-[50px]   hover:scale-110 duration-150     " src="src/images/Screenshot_2024-10-02_at_7.40.09_PM-removebg-preview.png" alt="" />
+                                <img className="sm:h-[80px] h-[50px]   hover:scale-110 duration-150     " src={word} alt="" />
                                 <h1 className=" mt-[20px] font-semibold  text-black  " > Wordpress </h1>
                             </div>
 
                             <div>
-                                <img className="sm:h-[80px]  h-[50px] hover:scale-110 duration-150    " src="src/images/Screenshot_2024-10-02_at_7.49.06_PM-removebg-preview.png" alt="" />
+                                <img className="sm:h-[80px]  h-[50px] hover:scale-110 duration-150    " src={APP} alt="" />
                                 <h1 className=" mt-[20px] font-semibold  text-black  " > Mobile App </h1>
                             </div>
                     </div>
@@ -173,19 +189,17 @@ function Home (){
                      <h1 className=" sm:ml-[100px] ml-[20px]  text-[25px] font-semibold  mt-[10px]  text-black " >OTHER SKILLS:</h1>
                     <div className="grid grid-cols-3 gap-[20px] sm:ml-[300px]  ml-[30px] pt-[30px]    " >
                             <div>
-                                <img className="sm:h-[80px]  h-[50px]   hover:scale-110 duration-150    " src="src/images/Screenshot_2024-10-02_at_8.14.18_PM-removebg-preview.png" alt="" />
+                                <img className="sm:h-[80px]  h-[50px]   hover:scale-110 duration-150    " src={ENGLISH} alt="" />
                                 <h1 className=" mt-[20px] font-semibold  text-black  " >English  </h1>
                             </div>
 
                             <div>
-                                <img className=" sm:h-[80px] h-[50px]   hover:scale-110 duration-150    " src="src/images/Screenshot_2024-10-02_at_8.12.11_PM-removebg-preview.png" alt="" />
+                                <img className=" sm:h-[80px] h-[50px]   hover:scale-110 duration-150    " src={ARABIC} alt="" />
                                 <h1 className=" mt-[20px] font-semibold  text-black  " >Arabic </h1>
                             </div>
                     </div>
                </div>
             </div>
-
-    
 
         </section>
         <section>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,16 +1,24 @@
 import { Link } from "react-router-dom"
+import logo from "../images/Screenshot_2024-10-01_at_1.21.59_PM-removebg-preview.png"
+import EXAM  from "../images/Screenshot 2024-10-03 at 10.06.31 AM.png"
+import E  from "../images/Screenshot 2024-10-03 at 9.57.57 AM.png"
+import APP from "../images/Screenshot 2024-10-03 at 1.41.25 PM.png"
+import NOTE from "../images/Screenshot 2024-10-03 at 1.51.29 PM.png"
+
+
 
 function Project (){
     return <div>
         <div className=" bg-black  h-[80px] w-full flex justify-around  " >
-                    <img id="UL"  className= " h-[50px]  mt-[10px] sm:ml-[-109px]   " src="src/images/Screenshot_2024-10-01_at_1.21.59_PM-removebg-preview.png" alt="" />
-                    <ul id="UL"  className="text-[white] ml-[100px] flex items-center gap-[50px]  " >
-                        {/* <li className=" hover:scale-110  hover:border-b-[1px] border-[white] duration-150  " > <a href="#About">ABOUT ME</a> </li> */}
-                       <Link to="/" > <li className=" hover:scale-110  hover:border-b-[1px] border-[white]   duration-150  " > HOME</li></Link>
-                        {/* <li className=" hover:scale-110  hover:border-b-[1px] border-[white] duration-150  " > <a href="#Skills">MY PROJECTS</a></li> */}
-                       {/* <Link  to="Contact" > <button className="bg-white text-black h-[30px] w-[130px] hover:scale-110 duration-150  rounded-[5px]   " >CONTACT ME</button></Link> */}
-                    </ul>
-                </div>
+
+                <img id="UL"  className= " h-[50px]  mt-[10px] sm:ml-[-109px]   " src={logo} alt="" />
+                        <ul id="UL"  className="text-[white] ml-[100px] flex items-center gap-[50px]  " >
+                            {/* <li className=" hover:scale-110  hover:border-b-[1px] border-[white] duration-150  " > <a href="#About">ABOUT ME</a> </li> */}
+                        <Link to="/" > <li className=" hover:scale-110  hover:border-b-[1px] border-[white]   duration-150  " > HOME</li></Link>
+                            {/* <li className=" hover:scale-110  hover:border-b-[1px] border-[white] duration-150  " > <a href="#Skills">MY PROJECTS</a></li> */}
+                        {/* <Link  to="Contact" > <button className="bg-white text-black h-[30px] w-[130px] hover:scale-110 duration-150  rounded-[5px]   " >CONTACT ME</button></Link> */}
+                        </ul>
+        </div>
 
         <div>
         <h1 className="  font-Body text-[40px] font-semibold h-[60px]  sm:h-[100px] rounded-[2px] w-[200px]  ml-[100px]  sm:w-[400px] sm:pt-[10px]   sm:ml-[530px] mt-[30px] text-center border-[4px] border-black  " > Projects </h1>
@@ -20,21 +28,21 @@ function Project (){
         <div   className=" grid grid-cols-1 sm:grid-cols-3  gap-[20px] sm:ml-[20px] mt-[20px]  "  >
             <div id="me"  className=" border-[white] text-center border-[3px] bg-black  rounded-[10px]  h-[300px]  "   >
                 <h1 className=" text-white font-Body font-semibold text-[20px] pt-[10px]  " >Exam Center</h1>
-                <img className="sm:h-[200px]  h-[180px]   mt-[20px] ml-[40px] hover:scale-105 duration-150     " src="src/images/Screenshot 2024-10-03 at 10.06.31 AM.png" alt="" />
+                <img className="sm:h-[200px]  h-[180px]   mt-[20px] ml-[40px] hover:scale-105 duration-150     " src={EXAM} alt="" />
             </div>
 
             <div id="me" className=" border-[white] text-center border-[3px] bg-black  rounded-[10px]  h-[300px]  "  >
                 <h1 className=" text-white font-Body font-semibold text-[20px] pt-[10px]  " >E-Commerce</h1>
-                <img className="sm:h-[200px]  h-[180px]  mt-[20px] ml-[40px] hover:scale-105 duration-150      " src="src/images/Screenshot 2024-10-03 at 9.57.57 AM.png " alt="" />
+                <img className="sm:h-[200px]  h-[180px]  mt-[20px] ml-[40px] hover:scale-105 duration-150      " src={E} alt="" />
             </div>
 
             <div id="me"  className=" border-[white] text-center border-[3px] bg-black  rounded-[10px]  h-[300px]  " >
                 <h1 className=" text-white font-Body font-semibold text-[20px] pt-[10px]  " >Voting App</h1>
-                <img className="sm:h-[200px]  h-[180px] mt-[20px]  ml-[40px] hover:scale-105 duration-150      " src="src/images/Screenshot 2024-10-03 at 1.41.25 PM.png" alt="" />
+                <img className="sm:h-[200px]  h-[180px] mt-[20px]  ml-[40px] hover:scale-105 duration-150      " src={APP} alt="" />
             </div  >
             <div id="me" className=" border-[white] text-center border-[3px] bg-black  rounded-[10px]  h-[300px]  " >
                 <h1 className=" text-white font-Body font-semibold text-[20px] pt-[10px]  " >Note Taker App</h1>
-                <img className="sm:h-[200px]  h-[180px] mt-[20px]  ml-[40px] hover:scale-105 duration-150      " src="src/images/Screenshot 2024-10-03 at 1.51.29 PM.png" alt="" />
+                <img className="sm:h-[200px]  h-[180px] mt-[20px]  ml-[40px] hover:scale-105 duration-150      " src={NOTE} alt="" />
             </div  >
             <div id="me" className=" border-[white] text-center border-[3px]   rounded-[10px]  h-[300px]  " >
                 <h1 className=" text-black font-Body font-semibold text-[20px] pt-[100px]  " > <a href="https://github.com/sudeys-1"> See More... <i class="fa-solid fa-chevron-right fa-beat-fade"></i> </a>  </h1>
