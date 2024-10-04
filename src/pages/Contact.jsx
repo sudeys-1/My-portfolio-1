d5d2123f4aeaa2987259d64b307b553cadfe7825
sudeys <sudeysimaxamud5@gmail.com>
2024-10-04 18:58:27 +0300

    myPortfolio

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,59 +1,25 @@
-import { useState  } from "react"
-import axios from "axios"
-import { toast } from "react-hot-toast"
+// import { useState  } from "react"
+// import axios from "axios"
+// import { toast } from "react-hot-toast"
 import { Link } from "react-router-dom"
 function Contact (){
-    const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
-    const [massage, setMessage] = useState("")
+    // const [name, setName] = useState("")
+    // const [email, setEmail] = useState("")
+    // const [massage, setMessage] = useState("")
 
-    const handleFeedBack =(e)=>{
-        e.preventDefault()
-        axios.post("https://my-portfolio-backend-1-5ebv.onrender.com/Create/massages",{
-            "name" :name,
-            "Email" :email,
-            "massage" :massage
-        }).then(()=>{
+    // const handleFeedBack =(e)=>{
+    //     e.preventDefault()
+    //     axios.post("https://my-portfolio-backend-1-5ebv.onrender.com/Create/massages",{
+    //         "name" :name,
+    //         "Email" :email,
+    //         "massage" :massage
+    //     }).then(()=>{
         
-            toast.custom((t) => (
-                <div
-                  className={`${
-                    t.visible ? 'animate-enter' : 'animate-leave'
-                  } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
-                >
-                  <div className="flex-1 w-0 p-4">
-                    <div className="flex items-start">
-                      <div className="flex-shrink-0 pt-0.5">
-                        <img
-                          className="h-10 w-10 rounded-full"
-                          src="src/images/Untitled-NJXyBb8Qp-transformed 2.jpg"
-                          alt=""
-                        />
-                      </div>
-                      <div className="ml-3 flex-1">
-                        <p className="text-sm font-medium text-gray-900">
-                          Sudeys Mohamud
-                        </p>
-                        <p className="mt-1 text-sm text-gray-500">
-                        Thank you for your message! I will get back to you as soon as possible.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="flex border-l border-gray-200">
-                    <button
-                      onClick={() => toast.dismiss(t.id)}
-                      className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                    >
-                      Close
-                    </button>
-                  </div>
-                </div>
-              ))
-        }).catch((error)=>{
-            console.log(error)
-        })
-    }
+            
+    //     }).catch((error)=>{
+    //         console.log(error)
+    //     })
+    // }
 
 
     return <div className="font-Body " >
@@ -74,10 +40,10 @@ function Contact (){
 
             <div  className="  flex  justify-center" >
                 <form className=" flex flex-col gap-[50px] " >
-                    <input value={name}  onChange={(event)=> setName(event.target.value)}   className=" border-l-[3px] h-[30px] w-[300px]  sm:w-[400px] border-b-[3px] border-black pl-[10px]    "  type="text"  placeholder="Enter Your Name " />
-                    <input value={email}  onChange={(event)=> setEmail(event.target.value)}   className=" border-l-[3px] h-[30px] w-[300px] sm:w-[400px] border-b-[3px] border-black pl-[10px]    "  type="text"  placeholder="Enter Your Email" />
-                    <input value={massage}  onChange={(event)=> setMessage(event.target.value)}   className=" border-l-[3px] h-[30px]  w-[300px] sm:w-[400px] border-b-[3px] border-black pl-[10px]    "  type="text"  placeholder="Enter Your Message" />
-                    <button onClick={handleFeedBack} className=" h-[30px] w-[100px] bg-black text-white rounded-[3px] hover:scale-110 duration-150  " >   SEND </button>
+                    <input   className=" border-l-[3px] h-[30px] w-[300px]  sm:w-[400px] border-b-[3px] border-black pl-[10px]    "  type="text"  placeholder="Enter Your Name " />
+                    <input   className=" border-l-[3px] h-[30px] w-[300px] sm:w-[400px] border-b-[3px] border-black pl-[10px]    "  type="text"  placeholder="Enter Your Email" />
+                    <input  className=" border-l-[3px] h-[30px]  w-[300px] sm:w-[400px] border-b-[3px] border-black pl-[10px]    "  type="text"  placeholder="Enter Your Message" />
+                  <Link  to="/" >  <button  className=" h-[30px] w-[100px] bg-black text-white rounded-[3px] hover:scale-110 duration-150  " >   SEND </button></Link>
                 </form>
             </div>
 
